6e15e12d3fd48245c8387a6e48637a7d701ddfcf
Jason Olejarz <104961898+jolejarz@users.noreply.github.com>
2025-10-06 17:45:56 -0400

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -1,1 +1,1 @@
-nasm -felf64 mt19937.asm && nasm -felf64 test.asm && ld mt19937.o test.o -o test
+nasm -felf64 mt19937.asm && nasm -felf64 test_asm.asm && ld mt19937.o test_asm.o -o test_asm && gcc -z noexecstack mt19937.o test_c.c -o test_c
